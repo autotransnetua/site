3fe9390de0299b59338f8bd7b85e3f30cac186a8
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2026-05-13 21:34:06 +0300

    оновлено robots & sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,8 @@
-// import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
-// export default function robots(): MetadataRoute.Robots {
-// 	return {
-// 		rules: { userAgent: '*', allow: '/' },
-// 		sitemap: 'https://autotrans.com.ua/sitemap.xml'
-// 	}
-// }
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: { userAgent: '*', allow: '/' },
+		sitemap: 'https://autotrans.com.ua/sitemap.xml'
+	}
+}
